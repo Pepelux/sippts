2b9726b72ce380ecf0bb41e1efef2a46933b1b8e
Jose Luis Verdeguer <pepeluxx@gmail.com>
2022-11-10 10:12:44 +0100

    New version 3.3

--- modules/sipflood.py
+++ modules/sipflood.py
@@ -2,12 +2,11 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Jose Luis Verdeguer'
-__version__ = '3.2'
+__version__ = '3.3'
 __license__ = "GPL"
 __copyright__ = "Copyright (C) 2015-2022, SIPPTS"
 __email__ = "pepeluxx@gmail.com"
 
-from logging.handlers import NTEventLogHandler
 import os
 import socket
 import signal
