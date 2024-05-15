3343fd1d058bc761a08cc4ddfad87709dd5d2d61
Jose Luis Verdeguer <pepelux@MacBook-Pro-de-Jose.local>
2024-05-15 19:35:20 +0200

    v4.0

--- setup.py
+++ setup.py
@@ -3,27 +3,11 @@
 from setuptools import setup
 
 scripts=[
-  "bin/arpspoof",
-  "bin/rtcpbleed",
-  "bin/rtpbleed",
-  "bin/rtpbleedflood",
-  "bin/rtpbleedinject",
-  "bin/sipdigestcrack",
-  "bin/sipdigestleak",
-  "bin/sippcapdump",
-  "bin/sipenumerate",
-  "bin/sipexten",
-  "bin/sipflood",
-  "bin/sipfuzzer",
-  "bin/sipinvite",
-  "bin/sipping",
-  "bin/siprcrack",
-  "bin/sipscan",
-  "bin/sipsend",
-  "bin/sipsniff",
-  "bin/siptshark",
-  "bin/wssend",
+  "bin/sippts"
 ]
 
 if __name__ == "__main__":
-  setup(scripts=scripts)
+  setup(scripts=scripts,
+        include_package_data=True,
+        package_data={'sippts': ['data/cve.csv']}
+        )
